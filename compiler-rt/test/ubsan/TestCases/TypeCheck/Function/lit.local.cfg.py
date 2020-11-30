a9d9115df4b2b1c4559a9a50adc0f5bd0feb6ff6
Luís Marques <luismarques@lowrisc.org>
2020-11-30 09:53:07 +0000

    WIP lsan RISC-V support

--- compiler-rt/test/ubsan/TestCases/TypeCheck/Function/lit.local.cfg.py
+++ compiler-rt/test/ubsan/TestCases/TypeCheck/Function/lit.local.cfg.py
@@ -1,3 +1,3 @@
 # The function type checker is only supported on x86 and x86_64 for now.
-if config.target_arch not in ['x86', 'x86_64']:
+if config.target_arch not in ['x86', 'x86_64', 'riscv64']:
   config.unsupported = True
