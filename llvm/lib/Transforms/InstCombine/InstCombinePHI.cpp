b79315a7071612621ac543cd7595cb487c437a64
Luís Marques <luis@luismarques.eu>
2023-01-25 17:07:17 +0000

    Review D142551

--- llvm/lib/Transforms/InstCombine/InstCombinePHI.cpp
+++ llvm/lib/Transforms/InstCombine/InstCombinePHI.cpp
@@ -973,23 +973,62 @@ Instruction *InstCombinerImpl::foldPHIArgOpIntoPHI(PHINode &PN) {
 }
 
 /// Return true if this PHI node is only used by a PHI node cycle that is dead.
-static bool isDeadPHICycle(PHINode *PN,
+/// For example, in the following graph of instructions there is a cycle at ϕ.A
+/// between ϕ.A and ϕ.B that isn't used by another instructions.
+/// ┌─────┐
+/// │  a  │
+/// └─────┘
+///   │
+///   │
+///   ▼
+/// ┌─────┐
+/// │ ϕ.A │ ◀┐
+/// └─────┘  │
+///   │      │
+///   │      │
+///   ▼      │
+/// ┌─────┐  │
+/// │  x  │ ─┘
+/// └─────┘
+///   │
+///   │
+///   ▼
+/// ┌─────┐
+/// │ ϕ.B │ ◀┐
+/// └─────┘  │
+///   │      │
+///   │      │
+///   ▼      │
+/// ┌─────┐  │
+/// │  y  │ ─┘
+/// └─────┘
+static bool isDeadPHICycle(Instruction *I,
                            SmallPtrSetImpl<PHINode *> &PotentiallyDeadPHIs) {
-  if (PN->use_empty()) return true;
-  if (!PN->hasOneUse()) return false;
+  if (PHINode *PN = dyn_cast<PHINode>(I)) {
+    // A PHI node with no uses is dead.
+    if (PN->use_empty())
+      return true;
 
-  // Remember this node, and if we find the cycle, return.
-  if (!PotentiallyDeadPHIs.insert(PN).second)
-    return true;
+    // Remember this node, and if we find the cycle, return.
+    if (!PotentiallyDeadPHIs.insert(PN).second)
+      return true;
 
-  // Don't scan crazily complex things.
-  if (PotentiallyDeadPHIs.size() == 16)
+    // Don't scan crazily complex things.
+    if (PotentiallyDeadPHIs.size() == 16)
+      return false;
+  }
+
+  // An instruction is in a dead PHI cycle iff it has at least one user that is
+  // also in a cycle.
+  if (I->use_empty() || I->mayHaveSideEffects())
     return false;
 
-  if (PHINode *PU = dyn_cast<PHINode>(PN->user_back()))
-    return isDeadPHICycle(PU, PotentiallyDeadPHIs);
-
-  return false;
+  return all_of(I->users(), [&PotentiallyDeadPHIs](User *U) {
+    Instruction *UI = dyn_cast<Instruction>(U);
+    if (!UI)
+      return false;
+    return isDeadPHICycle(UI, PotentiallyDeadPHIs);
+  });
 }
 
 /// Return true if this phi node is always equal to NonPhiInVal.
@@ -1415,32 +1454,17 @@ Instruction *InstCombinerImpl::visitPHINode(PHINode &PN) {
     }
   }
 
-  // If this is a trivial cycle in the PHI node graph, remove it.  Basically, if
-  // this PHI only has a single use (a PHI), and if that PHI only has one use (a
-  // PHI)... break the cycle.
+  // If there is a cycle in the PHI node graph which is a dead, remove it.
+  // I.e. if this PHI has a use that is another PHI, and that PHI uses the first
+  // phi, and there are no other uses outside of this cycle, then break it.
+  SmallPtrSet<PHINode*, 16> PotentiallyDeadPHIs;
+  if (isDeadPHICycle(&PN, PotentiallyDeadPHIs))
+    return replaceInstUsesWith(PN, PoisonValue::get(PN.getType()));
+
   if (PN.hasOneUse()) {
     if (foldIntegerTypedPHI(PN))
       return nullptr;
 
-    Instruction *PHIUser = cast<Instruction>(PN.user_back());
-    if (PHINode *PU = dyn_cast<PHINode>(PHIUser)) {
-      SmallPtrSet<PHINode*, 16> PotentiallyDeadPHIs;
-      PotentiallyDeadPHIs.insert(&PN);
-      if (isDeadPHICycle(PU, PotentiallyDeadPHIs))
-        return replaceInstUsesWith(PN, PoisonValue::get(PN.getType()));
-    }
-
-    // If this phi has a single use, and if that use just computes a value for
-    // the next iteration of a loop, delete the phi.  This occurs with unused
-    // induction variables, e.g. "for (int j = 0; ; ++j);".  Detecting this
-    // common case here is good because the only other things that catch this
-    // are induction variable analysis (sometimes) and ADCE, which is only run
-    // late.
-    if (PHIUser->hasOneUse() &&
-        (isa<BinaryOperator>(PHIUser) || isa<GetElementPtrInst>(PHIUser)) &&
-        PHIUser->user_back() == &PN) {
-      return replaceInstUsesWith(PN, PoisonValue::get(PN.getType()));
-    }
     // When a PHI is used only to be compared with zero, it is safe to replace
     // an incoming value proved as known nonzero with any non-zero constant.
     // For example, in the code below, the incoming value %v can be replaced
@@ -1449,7 +1473,7 @@ Instruction *InstCombinerImpl::visitPHINode(PHINode &PN) {
     // %v = select %cond, 1, 2
     // %p = phi [%v, BB] ...
     //      icmp eq, %p, 0
-    auto *CmpInst = dyn_cast<ICmpInst>(PHIUser);
+    auto *CmpInst = dyn_cast<ICmpInst>(PN.user_back());
     // FIXME: To be simple, handle only integer type for now.
     if (CmpInst && isa<IntegerType>(PN.getType()) && CmpInst->isEquality() &&
         match(CmpInst->getOperand(1), m_Zero())) {
