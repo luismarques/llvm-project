921ed169fbde0005369d10cc0ff81dce1802aece
Luís Marques <luismarques@lowrisc.org>
2023-04-06 12:03:32 +0100

    [RISCV] Support 0p93 suffix in '.option arch' arch string

--- llvm/lib/Target/RISCV/AsmParser/RISCVAsmParser.cpp
+++ llvm/lib/Target/RISCV/AsmParser/RISCVAsmParser.cpp
@@ -3127,9 +3127,10 @@ bool RISCVAsmParser::parseDirectiveOption() {
         break;
       }
 
+      Arch.consume_back("0p93");
       if (isDigit(Arch.back()))
         return Error(
-            Loc, "extension version number parsing not currently implemented");
+            Loc, "invalid extension version number");
 
       std::string Feature = RISCVISAInfo::getTargetFeatureForExtension(Arch);
       if (!enableExperimentalExtension() &&
